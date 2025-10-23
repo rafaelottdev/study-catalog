a8d481fb10aeaa24f43b30f26f5c7185153a756b
Rafael Ott <157972006+rafaelottdev@users.noreply.github.com>
2025-10-23 19:56:46 -0300

    feat(logo): click and scroll to top

--- src/layouts/Header/Logo/Logo.jsx
+++ src/layouts/Header/Logo/Logo.jsx
@@ -2,11 +2,23 @@ import { Link } from "react-router-dom"
 import styles from "./Logo.module.css"
 
 const Logo = ({ isScrolling, show, setShow }) => {
+    const scrollToTop = () => {
+        window.scrollTo(
+            {
+                top: 0,
+                behavior: 'smooth'
+            }
+        )
+    }
+
     return (
         <div className={styles.logo_wrapp}>
             <Link to="/" 
                 className={`${styles.logo_link} ${isScrolling ? `${styles.scrolling}` : ""}`}
-                onClick={() => setShow(false)}
+                onClick={() => {
+                    setShow(false)
+                    scrollToTop()
+                }}
             >
                 <h1 className={styles.logo_title}>Study Catalog</h1>
             </Link>
