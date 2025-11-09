6feebbe15f239936f3f0cabfc6441d005cbea998
Rafael Ott <157972006+rafaelottdev@users.noreply.github.com>
2025-11-09 10:26:26 -0300

    fix(studynavbar): fix StudyNavbar css path

--- src/pages/Study/StudyNavbar/StudyNavbar.jsx
+++ src/pages/Study/StudyNavbar/StudyNavbar.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom"
-import styles from "./studyNavbar.module.css"
+import styles from "./StudyNavbar.module.css"
 import StudySubList from "./StudySubList/StudySubList"
 import { useState } from "react"
 
