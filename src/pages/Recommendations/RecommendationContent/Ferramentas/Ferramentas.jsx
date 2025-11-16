ad97ed2bf5675eca3b70710fd0824f53d3ff7446
Rafael Ott <157972006+rafaelottdev@users.noreply.github.com>
2025-11-16 18:05:51 -0300

    style(recommendation): styling recommendation links

--- src/pages/Recommendations/RecommendationContent/Ferramentas/Ferramentas.jsx
+++ src/pages/Recommendations/RecommendationContent/Ferramentas/Ferramentas.jsx
@@ -1,123 +1,123 @@
-import styles from "./Ferramentas.module.css"
+import styles from "../RecommendationContent.module.css"
 
 const Ferramentas = () => {
     return (
-        <section>
-            <ul>
+        <section className={styles.recommendation_container}>
+            <ul className={styles.recommendation_list}>
                 <li>
-                    <a href="https://ttsmaker.com/">Narrador de Texto</a>
+                    <a href="https://ttsmaker.com/" target="_blank">Narrador de Texto</a>
                 </li>
 
                 <li>
-                    <a href="https://123apps.com/pt/">123APPS</a>
+                    <a href="https://123apps.com/pt/" target="_blank">123APPS</a>
                 </li>
 
                 <li>
-                    <a href="https://getcssscan.com/css-box-shadow-examples">CSS box-shadows</a>
+                    <a href="https://getcssscan.com/css-box-shadow-examples" target="_blank">CSS box-shadows</a>
                 </li>
 
                 <li>
-                    <a href="https://looka.com/logo-maker/">Logo Maker</a>
+                    <a href="https://looka.com/logo-maker/" target="_blank">Logo Maker</a>
                 </li>
 
                 <li>
-                    <a href="https://getwaves.io/">Waves</a>
+                    <a href="https://getwaves.io/" target="_blank">Waves</a>
                 </li>
 
                 <li>
-                    <a href="https://pfpmaker.com/">Fotos de Perfil Com IA</a>
+                    <a href="https://pfpmaker.com/" target="_blank">Fotos de Perfil Com IA</a>
                 </li>
 
                 <li>
-                    <a href="https://easings.net/">Animações Ease CSS</a>
+                    <a href="https://easings.net/" target="_blank">Animações Ease CSS</a>
                 </li>
 
                 <li>
-                    <a href="https://tinypng.com/">Compressor de Imagens</a>
+                    <a href="https://tinypng.com/" target="_blank">Compressor de Imagens</a>
                 </li>
 
                 <li>
-                    <a href="https://loading.io/">Animações CSS</a>
+                    <a href="https://loading.io/" target="_blank">Animações CSS</a>
                 </li>
 
                 <li>
-                    <a href="https://editor.method.ac/">Criador de Vetor</a>
+                    <a href="https://editor.method.ac/" target="_blank">Criador de Vetor</a>
                 </li>
 
                 <li>
-                    <a href="https://en1.savefrom.net/11Ox/sf">Video Downloader</a>
+                    <a href="https://en1.savefrom.net/11Ox/sf" target="_blank">Video Downloader</a>
                 </li>
 
                 <li>
-                    <a href="https://9elements.github.io/fancy-border-radius">Criador de Border Radius</a>
+                    <a href="https://9elements.github.io/fancy-border-radius" target="_blank">Criador de Border Radius</a>
                 </li>
 
                 <li>
-                    <a href="https://cssbud.com/css-generator/css-glow-generator/">Gerador de Glow CSS</a>
+                    <a href="https://cssbud.com/css-generator/css-glow-generator/" target="_blank">Gerador de Glow CSS</a>
                 </li>
 
                 <li>
-                    <a href="https://animista.net/">Animações CSS</a>
+                    <a href="https://animista.net/" target="_blank">Animações CSS</a>
                 </li>
 
                 <li>
-                    <a href="https://www.photopea.com/">Editor de Imagens</a>
+                    <a href="https://www.photopea.com/" target="_blank">Editor de Imagens</a>
                 </li>
 
                 <li>
-                    <a href="https://color.adobe.com/pt/create/color-wheel">Cores</a>
+                    <a href="https://color.adobe.com/pt/create/color-wheel" target="_blank">Cores</a>
                 </li>
 
                 <li>
-                    <a href="https://fonts.google.com/">Google Fonts</a>
+                    <a href="https://fonts.google.com/" target="_blank">Google Fonts</a>
                 </li>
 
                 <li>
-                    <a href="https://www.dafont.com/pt/">DaFont</a>
+                    <a href="https://www.dafont.com/pt/" target="_blank">DaFont</a>
                 </li>
 
                 <li>
-                    <a href="https://www.whatfontis.com/#google_vignette">Font Finder</a>
+                    <a href="https://www.whatfontis.com/#google_vignette" target="_blank">Font Finder</a>
                 </li>
 
                 <li>
-                    <a href="https://y2down.cc/ptPo/">Youtube Downloader</a>
+                    <a href="https://y2down.cc/ptPo/" target="_blank">Youtube Downloader</a>
                 </li>
 
                 <li>
-                    <a href="https://trello.com/">Trello</a>
+                    <a href="https://trello.com/" target="_blank">Trello</a>
                 </li>
 
                 <li>
-                    <a href="https://app.animaker.com/">Gerador de Animação</a>
+                    <a href="https://app.animaker.com/" target="_blank">Gerador de Animação</a>
                 </li>
 
                 <li>
-                    <a href="https://app.sketchup.com/app">3D</a>
+                    <a href="https://app.sketchup.com/app" target="_blank">3D</a>
                 </li>
 
                 <li>
-                    <a href="https://excalidraw.com/">Excalidraw</a>
+                    <a href="https://excalidraw.com/" target="_blank">Excalidraw</a>
                 </li>
 
                 <li>
-                    <a href="https://icons8.com/icons">Icons8</a>
+                    <a href="https://icons8.com/icons" target="_blank">Icons8</a>
                 </li>
 
                 <li>
-                    <a href="https://www.capcut.com/?enter_from=page_header&from_page=editor&position=my_draft&start_tab=video">Capcut</a>
+                    <a href="https://www.capcut.com/?enter_from=page_header&from_page=editor&position=my_draft&start_tab=video" target="_blank">Capcut</a>
                 </li>
 
                 <li>
-                    <a href="https://www.fontshare.com/">Fontshare</a>
+                    <a href="https://www.fontshare.com/" target="_blank">Fontshare</a>
                 </li>
 
                 <li>
-                    <a href="https://github.com/commitizen/cz-cli">Padronização de Commit</a>
+                    <a href="https://github.com/commitizen/cz-cli" target="_blank">Padronização de Commit</a>
                 </li>
 
                 <li>
-                    <a href="https://roadmap.sh/">Roadmaps</a>
+                    <a href="https://roadmap.sh/" target="_blank">Roadmaps</a>
                 </li>
             </ul>
         </section>
--- src/pages/Recommendations/RecommendationContent/Sites/Sites.jsx
+++ src/pages/Recommendations/RecommendationContent/Sites/Sites.jsx
@@ -1,35 +1,35 @@
-import styles from "./Sites.module.css"
+import styles from "../RecommendationContent.module.css"
 
 const Sites = () => {
     return (
-        <section>
-            <ul>
+        <section className={styles.recommendation_container}>
+            <ul className={styles.recommendation_list}>
                 <li>
-                    <a href="https://www.codewars.com/">Codewars</a>
+                    <a href="https://www.codewars.com/" target="_blank">Codewars</a>
                 </li>
 
                 <li>
-                    <a href="https://www.hackerrank.com/">HackerRank</a>
+                    <a href="https://www.hackerrank.com/" target="_blank">HackerRank</a>
                 </li>
 
                 <li>
-                    <a href="https://www.frontendmentor.io/">Frontend Mentor</a>
+                    <a href="https://www.frontendmentor.io/" target="_blank">Frontend Mentor</a>
                 </li>
 
                 <li>
-                    <a href="https://cssbattle.dev/">CssBattle</a>
+                    <a href="https://cssbattle.dev/" target="_blank">CssBattle</a>
                 </li>
 
                 <li>
-                    <a href="https://coderbyte.com/">CoderByte</a>
+                    <a href="https://coderbyte.com/" target="_blank">CoderByte</a>
                 </li>
 
                 <li>
-                    <a href="https://javascript30.com/">Javascript30</a>
+                    <a href="https://javascript30.com/" target="_blank">Javascript30</a>
                 </li>
 
                 <li>
-                    <a href="https://beecrowd.com/pt/">BeeCrowd</a>
+                    <a href="https://beecrowd.com/pt/" target="_blank">BeeCrowd</a>
                 </li>
             </ul>
         </section>
