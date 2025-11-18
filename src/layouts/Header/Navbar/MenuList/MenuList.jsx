3d8dcb43b43bbb2b32b259cc9b7f3ef791ea0333
Rafael Ott <157972006+rafaelottdev@users.noreply.github.com>
2025-11-17 21:32:56 -0300

    style(header): converting header style for desktop

--- src/layouts/Header/Navbar/MenuList/MenuList.jsx
+++ src/layouts/Header/Navbar/MenuList/MenuList.jsx
@@ -28,43 +28,46 @@ const MenuList = ({ show, setShow, burgerRef }) => {
 
     return (
         <ul  ref={menuRef} className={`${styles.nav_list} ${show ? `${styles.show_list}` : ""}`}>
-            <li className={styles.nav_item} onClick={() => setShow(!show)}>
-                <Link to="/estudando" className={styles.nav_link}>Estudos</Link>
-            </li>
+            <div className={styles.wrapp_items}>
+                <li className={styles.nav_item} onClick={() => setShow(!show)}>
+                    <Link to="/estudando" className={styles.nav_link}>Estudos</Link>
+                </li>
 
-            <li className={styles.nav_item}>
-                <Link to="/projetos" className={styles.nav_link} onClick={() => setShow(!show)}>Projetos</Link>
-            </li>
+                <li className={styles.nav_item}>
+                    <Link to="/projetos" className={styles.nav_link} onClick={() => setShow(!show)}>Projetos</Link>
+                </li>
 
-            <li className={styles.nav_item}>
-                <Link to="/recomendações" className={styles.nav_link} onClick={() => setShow(!show)}>Recomendações</Link>
-            </li>
+                <li className={styles.nav_item}>
+                    <Link to="/recomendações" className={styles.nav_link} onClick={() => setShow(!show)}>Recomendações</Link>
+                </li>
+            </div>
 
-            <li className={styles.nav_item}>
-                <Link to="/trabalhando" className={styles.nav_link} onClick={() => setShow(!show)}>Trabalhando</Link>
-            </li>
+            <div className={styles.wrapp_items}>
+                <li className={styles.nav_item}>
+                    <Link to="/trabalhando" className={styles.nav_link} onClick={() => setShow(!show)}>Trabalhando</Link>
+                </li>
 
-            <li className={styles.nav_item}>
-                <Link to="/blog" className={styles.nav_link} onClick={() => setShow(!show)}>Blog</Link>
-            </li>
+                <li className={styles.nav_item}>
+                    <Link to="/blog" className={styles.nav_link} onClick={() => setShow(!show)}>Blog</Link>
+                </li>
 
-            <li className={`${styles.nav_item} ${styles.nav_item_sublist}`}>
-                <a href="#" className={styles.nav_link} onClick={() => {setActive(!active)}}>
-                    Mais
+                <li className={`${styles.nav_item} ${styles.nav_item_sublist}`}>
+                    <a href="#" className={styles.nav_link} onClick={() => {setActive(!active)}}>
+                        Mais
+                        <div className={`${styles.more_icon_wrapp} ${active ? `${styles.rotate_arrow}`: ""}`}>
+                            <svg viewBox="0 0 20 26">
+                                <path d="M14.4036 14.4442L2.88031 26L0 23.1116L10.0831 13L0 2.88843L2.88031 0L14.4036 11.5558C14.7855 11.9389 15 12.4583 15 13C15 13.5417 14.7855 14.0611 14.4036 14.4442Z"/>
+                            </svg>
+                        </div>
+                    </a>
 
-                    <div className={`${styles.more_icon_wrapp} ${active ? `${styles.rotate_arrow}`: ""}`}>
-                        <svg viewBox="0 0 20 26">
-                            <path d="M14.4036 14.4442L2.88031 26L0 23.1116L10.0831 13L0 2.88843L2.88031 0L14.4036 11.5558C14.7855 11.9389 15 12.4583 15 13C15 13.5417 14.7855 14.0611 14.4036 14.4442Z"/>
-                        </svg>
-                    </div>
-                </a>
-
-                <ul className={`${styles.more_sublist} ${active ? `${styles.show_sublist}` : ""}`}>
-                    <li className={styles.more_sublist_item}>
-                        <Link to="/comunidade" className={styles.more_sublist_link} onClick={() => setShow(!show)}>Comunidade</Link>
-                    </li>
-                </ul>
-            </li>
+                    <ul className={`${styles.more_sublist} ${active ? `${styles.show_sublist}` : ""}`}>
+                        <li className={styles.more_sublist_item}>
+                            <Link to="/comunidade" className={styles.more_sublist_link} onClick={() => setShow(!show)}>Comunidade</Link>
+                        </li>
+                    </ul>
+                </li>
+            </div>
         </ul>
     )
 }
